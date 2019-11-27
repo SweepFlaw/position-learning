40b18f1534a6613e3925fce51fa85e46f5a45b37
jisung hong <rhfktj@gmail.com>
2019-11-27 13:57:52 +0900

    Change filename by using dirname

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from position_learning import getRecommendList, saveName, embedCSV
+import os
 
 if __name__ == "__main__":
-  filename = '/newdisk/autofix/deeplearning/data/code.cpp'
+  filename = os.path.dirname(os.path.realpath(__file__)) + '/data/code.cpp'
   recommendList = getRecommendList(filename)
   print(recommendList)
